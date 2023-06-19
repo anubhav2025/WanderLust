b1d9602f2e55786cad8051a777fdfe782cb0debd
Anubhav Pandey <akp99376@gmail.com>
2023-06-20 00:02:36 +0530

    added seperate pages for each category

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -41,6 +41,31 @@ exports.exploreCategories = async(req, res) => {
     }
 }
 
+
+
+/* 
+* GET /categories/:id
+* Categories By Id
+*/
+exports.exploreCategoriesById = async(req, res) => {
+    try{
+        let categoryId = req.params.id;
+        const limitNumber = 20;
+        const categoryById = await Blog.find({ 'category': categoryId }).limit(limitNumber);
+        res.render('categories', {title: 'Cooking Blog - Categories', categoryById});
+    }
+    catch(error){
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+}
+
+
+
+
+
+
+
+
 /* 
 * GET /blod/:id
 * Blog
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,7 +4,8 @@ const recipeController = require('../controller/recipeController');
 
 // App routes
 router.get('/', recipeController.homePage); //homePage is controller name
-router.get('/categories', recipeController.exploreCategories);
 router.get('/blogs/:id', recipeController.exploreBlog); 
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
 
 module.exports = router;
